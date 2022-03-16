878c83f989064337fa4485f788dad476966b6ef3
Hansi <99313529+IT21355264@users.noreply.github.com>
2022-03-16 17:46:24 +0000

    complete

--- Student.h
+++ Student.h
@@ -1,9 +1,15 @@
 class Student {
   // private section
+privet:
   //    int studentId
+       int studentId;
   //    name <- 20 charcters
-  
+        char studentName[20];
   // public section
+public:
   //    assignDetails() method declaration
+      void assignDtails(int no,char name[]);
   //    display() method declaration
+      void display();
 };
+
